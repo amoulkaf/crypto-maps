b9850027e04a6c4a9833ec72b0d2c2e166f0fc1c
amoulkaf <anouar.moulkaf@gmail.com>
2018-05-08 14:33:07 -0700

    [WIP]feat(project):data template)

--- src/contract/cryptomaps.py
+++ src/contract/cryptomaps.py
@@ -1,3 +1,20 @@
 from boa.interop.Neo.Runtime import CheckWitness, Log
 from boa.interop.Neo.Storage import GetContext, Put, Delete, Get
+from boa.interop.Neo.Runtime import Notify, Serialize, Deserialize
+ctx = GetContext()
+
 from boa.builtins import concat
+
+
+def Main(operation, args):
+
+    # Common definitions
+    user_hash = args[0]
+    country = args[1]
+    city = args[2]
+
+    # This doesn't require authentication
+    if opperations == 'GetPlace':
+        return Get(GetContext(), coutry + city)
+
+    if opperations == 'registerPlace':
